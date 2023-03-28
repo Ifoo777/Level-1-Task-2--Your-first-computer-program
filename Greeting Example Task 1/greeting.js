ceeaa8049aeea9edc189ba0724174667073d0c47
Irfaan Osman <49390798+Ifoo777@users.noreply.github.com>
2023-03-28 11:59:02 +0200

    Add files via upload

---
+++ Greeting Example Task 1/greeting.js
@@ -0,0 +1,7 @@
+// Code here for Compulsory Task 1
+let myName = prompt("What is your name?");
+console.log ("Hi there , " + myName);
+
+let myAge = prompt("What is your age");
+console.log ("Your age is ," + myAge);
+console.log ("Its nice to meet you");
