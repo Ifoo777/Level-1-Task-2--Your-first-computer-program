15418ccede31ae6b0f3efa8910f8ea03697eefe6
Irfaan Osman <49390798+Ifoo777@users.noreply.github.com>
2023-03-28 11:59:18 +0200

    Add files via upload

---
+++ Shopping Example Task 2/shopping.js
@@ -0,0 +1,9 @@
+// Code here for Compulsory Task 2
+let myItem1 = ("Milk");
+let myItem2 = ("Lettuce");
+let myItem3 = ("Frozen Pizza");
+
+console.log("My shopping list:");
+console.log(myItem1);
+console.log(myItem2);
+console.log(myItem3);
